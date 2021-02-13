ec4e47a9058fc8b7b7b74272bd0d3eed6b0482d7
keanuc <keanuc@sfu.ca>
2021-02-13 11:18:52 -0800

    Added message to help command

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound
 
-client = discord.Bot(command_prefix = '/')
+client = commands.Bot(command_prefix = '/')
 client.remove_command("help")
 
 @client.event
@@ -12,5 +12,6 @@ async def on_ready():
 @client.command()
 async def help(ctx, *args):
     argCount = len(args)
+    await ctx.send("Nothing implemented here. Test message.")
 
 client.run('ODEwMjIzMjE1NjQwNTc2MDEw.YCghKw.R3qcwbgsQVSBhPwX2nl8F4tk73Y')
