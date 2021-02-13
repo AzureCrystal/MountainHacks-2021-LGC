391c35aa855b494f7c8330715b2c1c355a899d44
unknown <williamtj2001@gmail.com>
2021-02-13 11:34:28 -0800

    removed discord.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 import discord
+import json
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound
 
 client = commands.Bot(command_prefix = '/')
 client.remove_command("help")
+
+x = {
+  "books": [
+    {"name": "BMW 230"},
+    {"name": "Ford Edge"}
+  ]
+}
 
 @client.event
 async def on_ready():
@@ -12,6 +20,6 @@ async def on_ready():
 @client.command()
 async def help(ctx, *args):
     argCount = len(args)
-    await ctx.send("Nothing implemented here. Test message.")
+    await ctx.send("Nothing here")
 
 client.run('ODEwMjIzMjE1NjQwNTc2MDEw.YCghKw.R3qcwbgsQVSBhPwX2nl8F4tk73Y')
