21044d37870b633a95326e94a83fe13cd45bddd0
keanuc <keanuc@sfu.ca>
2021-02-13 11:15:01 -0800

    Added command prefix and help function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import CommandNotFound
 
-client = discord.Client()
+client = discord.Bot(command_prefix = '/')
+client.remove_command("help")
 
 @client.event
 async def on_ready():
     print("Bot is ready.")
+
+@client.command()
+async def help(ctx, *args):
+    argCount = len(args)
 
 client.run('ODEwMjIzMjE1NjQwNTc2MDEw.YCghKw.R3qcwbgsQVSBhPwX2nl8F4tk73Y')
