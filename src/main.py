8f99066db774c71362201ab15f5d89f4f5dc85af
AzureCrystal <email4keanu@gmail.com>
2021-02-13 13:34:17 -0800

    Fixed files not being found

--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 import discord
 import json
+import os
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound
 
 client = commands.Bot(command_prefix = '/')
 client.remove_command("help")
 
-tokenFile = open('token.txt', 'r')
+directory = os.path.dirname(os.path.abspath(__file__))
+tokenPath = os.path.join(directory, 'token.txt')
+
+tokenFile = open(tokenPath, 'r')
 token = tokenFile.read()
 
 # googleAPIKeyFile = open('googleApiKey.txt', 'r')
 # googleAPIKey = googleAPIKeyFile.read()
 
-jsonBookList = open('books.json')
+bookPath = os.path.join(directory, 'books.json')
+jsonBookList = open(bookPath)
 bookList = {}
 
 extensionList = ['cogs.help']
