bfb5d615ddd3ab65e1e197f6fa1cf9118a68d010
unknown <williamtj2001@gmail.com>
2021-02-13 13:05:43 -0800

    Added the google api key

--- src/main.py
+++ src/main.py
@@ -11,6 +11,9 @@ client.remove_command("help")
 tokenFile = open('token.txt', 'r')
 token = tokenFile.read()
 
+googleAPIKeyFile = open('googleApiKey.txt', 'r')
+googleAPIKey = googleAPIKeyFile.read()
+
 jsonBookList = open('books.json')
 bookList = {}
 
