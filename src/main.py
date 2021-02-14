a8f2a29025d8271ca288f38ff197d6723cd9da6c
Blamelessdeer <sz.rions@gmail.com>
2021-02-13 23:39:52 -0800

    Main changes

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
 import discord
 import requests
 import os
-<<<<<<< HEAD
-
-=======
 import json
->>>>>>> 191cc965da8a9db7038de57b52e52e0ab92d9009
+
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound
 from dotenv import load_dotenv
@@ -14,11 +11,6 @@ load_dotenv()
 client = commands.Bot(command_prefix = '/')
 client.remove_command("help")
 
-<<<<<<< HEAD
-directory = os.path.dirname(os.path.abspath(__file__))
-
-=======
->>>>>>> 191cc965da8a9db7038de57b52e52e0ab92d9009
 token = os.getenv('DISCORD_TOKEN')
 
 bookList = {}
