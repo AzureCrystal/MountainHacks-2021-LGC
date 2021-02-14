42bafe7036c4d67e397fd13ad449845cab8a9590
AzureCrystal <email4keanu@gmail.com>
2021-02-13 20:07:06 -0800

    Rewrote search.py

--- src/cogs/search.py
+++ src/cogs/search.py
@@ -18,11 +18,13 @@ class Search(commands.Cog):
         descWordCap = 1000
         if args[0] in parameters:
             response = requests.get("https://www.googleapis.com/books/v1/volumes?q=" + args[0] + ":" + '\"' + str(args[1]) + '\"')
-            if (int(len(args)) < 3):
-                if args[2].isnumeric():
-                    bookListLength = (args[2] + 1)
-            for booksIterator in range(bookListLength):
-                if "items" in response.json():
+            if "items" in response.json():
+                if (len(args) == 3):
+                    if args[2].isnumeric():
+                        bookListLength = (int(args[2]) + 1)
+                for booksIterator in range(bookListLength):
+                    print(booksIterator)
+                    
                     googleBooksList = response.json()['items']
                     result = "```\n"
                     if "title" in googleBooksList[booksIterator]["volumeInfo"]:
@@ -56,14 +58,11 @@ class Search(commands.Cog):
                             listVar = data["books"]
                             tempVar = {"name": bookName}
                             listVar.append(tempVar)
+                            await ctx.send("Book Added!")
                         with open(bookPath, 'w') as f:
                             json.dump(data, f, indent = 4)
-                else:
-                    if booksIterator == 0:
-                        await ctx.send("No books found.")
-                    else:
-                        await ctx.send("No more books found.")
-                    break
+            else:
+                ctx.send("No books found.")
         else:
             await ctx.send("Invalid parameters. Use: /search <type> <query>")
 
